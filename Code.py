134915d4491164661ac0cc98732c46c4dfb1aff5
Levyashin21 <89648968+Levyashin21@users.noreply.github.com>
2021-10-16 19:13:31 +0530

    Code

---
+++ Code.py
@@ -0,0 +1,15 @@
+
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup as bs
+from win10toast import ToastNotifier
+
+header = {"User-Agent":"Mozilla"}
+req = Request("https://www.worldometers.info/coronavirus/country/india/", headers = header)
+html = urlopen(req
+obj = bs(html)
+new_cases = obj.find("li", {"class":"news_li"}).strong.text.split()[0]
+death = list(obj.find("li", {"class":"news_li"}).strong.next_siblings)[1].text.split()[0]
+
+notifier = ToastNotifier()
+message  = "New Cases - "+ new_cases+"\nDCountry - "+death
+notifier.show_toast(title="COVID-19 Update", msg=message, duration=5, icon_path=r"virus.ico")
